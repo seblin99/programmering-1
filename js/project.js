786258c43acb2888ca7bc0c6963138e5da6ac9bd
Sebastian Lindqvist <sebastian.lindqvist@tranemo.org>
2017-02-27 15:57:22 +0100

    Måndag 27 feb

--- js/project.js
+++ js/project.js
@@ -5,27 +5,60 @@ var sketchProc = function(processingInstance) {
 		var canvas_size = 400
 		size(canvas_size ,canvas_size);
 		frameRate(50);
+		
+		// Bättre namn TODO
+		var q = 1;
+		var s = 2;
+		var b = 3;
 
-		//plane
-		stroke(0);
-
-		var draw = function (){
+		var background = function (){
+			fill (q,b,s);
+			// console.log("background");
+			rect(0, 0, canvas_size, canvas_size);
+			s+=0.2;
+			q+=0.3;
+			b+=0.7;
+			if(s+q+b> 600){
+				s = 100
+				q = 100
+				b = 100
+			}
+		}
+	
+		var playingfield = function () {
+			// Rita bakgrunden TODO
+			stroke(0);
 			line(0,390,400,390);
 			line(200,390,200,150);
+		}
 		
-		// p1 
-		var player1 = function(x){
-			line(x,100,x,390)
+		var player1 = function(x) {
+			line(x,100,x,390);
+
+			// console.log("Inuti player1");
+		}	
+		var movePlayer1 = function(step) {
+			// Ändra player1pos beroende på nedtryckt tangent
+			player1pos += step;
 		}
-
-
-	 	}
+// p1 
+		var keyPressed = function() {
+			  //  if (key == 'b' || key == 'B') {
+    				console.log("Key is pressed");
+			    	movePlayer1(2)
+			    //}
+			}
 		
-		}
+		// Globala variabler som delas mellan funktioner
+		var player1pos = 30;
+		var draw = function () {
+			background();
+			playingfield();
+		    player1(player1pos);
 
 		}
-    }
-};
+
+	}
+}
 var canvas = document.getElementById("mycanvas");
 var processingInstance= new Processing(canvas, sketchProc);
-
---
+++ testa-koden.js
@@ -0,0 +1,50 @@
+var sketchProc = function(processingInstance) {
+ with (processingInstance) {
+    size(400, 400); 
+    frameRate(50);
+ 
+    // ProgramCodeGoesHere
+    var xPosition = [ 200, 100, 90 ];
+    var yPosition = [ 100, 200, 250 ];
+    var xSpeed = [ 6, 3, 4 ];
+    var ySpeed = [ 3, 7, 5 ];
+    var faceSize = [ 100, 80, 60 ];
+    var drawFace = function(xPosition, yPosition, faceSize) {
+        fill(255, 255, 0);
+        ellipse(xPosition, yPosition, faceSize, faceSize);
+        noFill();
+        stroke(0, 0, 0);
+        strokeWeight(2);
+        arc(xPosition, yPosition, 0.75 * faceSize, 0.5 * faceSize, 0, PI);
+        fill(0, 0, 0);
+        ellipse(xPosition + faceSize/4, yPosition, faceSize/20, faceSize/20);
+        ellipse(xPosition - faceSize/4, yPosition, faceSize/20, faceSize/20);
+    }
+    var draw = function () {
+        background(49);
+        for ( let i = 0; i < xSpeed.length; i++ ) {
+            if ( xPosition[i] <= faceSize[i]/2 ) {
+                xSpeed[i] = Math.abs(xSpeed[i]);
+            }
+            if ( xPosition[i] >= 400 - faceSize[i]/2 ) {
+                xSpeed[i] = -Math.abs(xSpeed[i]);
+            }
+            if ( yPosition[i] <= faceSize[i]/2 ) {
+                ySpeed[i] = Math.abs(ySpeed[i]);
+            }
+            if ( yPosition[i] >= 400 - faceSize[i]/2 ) {
+                ySpeed[i] = -Math.abs(ySpeed[i]);
+            }
+
+            xPosition[i] += xSpeed[i];
+            yPosition[i] += ySpeed[i];
+            drawFace(xPosition[i], yPosition[i], faceSize[i]);
+        }
+    }
+}};
+
+
+// Get the canvas that Processing-js will use
+var canvas = document.getElementById("mycanvas"); 
+// Pass the function sketchProc (defined in myCode.js) to Processing's constructor.
+var processingInstance = new Processing(canvas, sketchProc);
